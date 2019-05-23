40669d60d25506ba39bf1d8d86be6f39f0d2dbb1
Grzegorz Zych <grzegorzzych95@gmail.com>
2019-05-23 18:06:56 +0200

    2nd version
    Signed-off-by: Grzegorz Zych <grzegorzzych95@gmail.com>

--- script.js
+++ script.js
@@ -4,30 +4,19 @@ const spanText = document.querySelector('span.text')
 let sentence = 0
 let letter = 0
 
-const pause = (ms) => {
-  const d = new Date()
-  let d2 = null
-  do {
-    d2 = new Date()
-  }
-  while (d2 - d < ms)
-}
-
 const typing = () => {
-  if (sentence === (text.length - 1) && letter === text[text.length - 1].length) {
-    clearInterval(interval)
-    return
+  if (letter === text[sentence].length) { //jesli litera ostatnia w wierszu
+    if (sentence === text.length - 1) return //jesli litera ostatnia i wiersz ostatni to wyjscie
+    return setTimeout(() => {
+      spanText.textContent = "" //jesli tylko litera ostatnia to nowy wiersz
+      letter = 0
+      sentence++
+      console.log('nowy wiersz')
+      setTimeout(typing, 500)
+    }, 2000) //przerwa na koniec wiersza
   }
-  if (letter === 0) pause(700)
   spanText.textContent += text[sentence][letter]
-  if (letter < text[sentence].length) {
-    letter++
-  } else {
-    sentence++;
-    letter = 0
-    spanText.textContent = ""
-    pause(1300)
-  }
+  letter++
+  setTimeout(typing, 100)
 }
-const interval = setInterval(typing, 50)
 typing()
